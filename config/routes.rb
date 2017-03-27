ebdbbd256c93a0603d55d8f1e8bfcc587ea1ac37
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-03-27 14:16:47 +0900

    add TasksController

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :builds, only: [:index, :show]
   resources :users, only: :index
   resources :repos, only: :index
+  resources :tasks, only: :index
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
