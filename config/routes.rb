eaff420a107ec4961ab86c4a063e6a6b2730fb9a
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-03-27 17:26:51 +0900

    add ProjectsController

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :builds, only: [:index, :show]
   resources :users, only: :index
   resources :repos, only: :index
-  resources :tasks, only: :index
+  resources :tasks, only: [:index, :new]
+  resources :projects, only: :new
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
