9f218d432514762fc5c81a0ac247bda010b5963e
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-03-27 19:43:48 +0900

    add SessionsController

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :repos, only: :index
   resources :tasks, only: [:index, :new]
   resources :projects, only: :new
+  resources :sessions, only: :index
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
